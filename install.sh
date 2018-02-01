c127aa55285af4be34459e073c56cba8890dd068
juantascon <juantascon@horlux.org>
2018-02-01 17:44:23 -0500

    install.sh installs i3 config

--- install.sh
+++ install.sh
@@ -16,11 +16,12 @@ lnk env/bash.d/bashrc .bash_profile
 lnk ../../env/fish.d/config.fish .config/fish/config.fish
 
 lnk env/python/pythonrc .pythonrc
-lnk env/tmux/tmux.conf .tmux.conf
 lnk env/vim .vim
 
 lnk env/xinit.d/xinitrc .xinitrc
 lnk env/xinit.d/xserverrc .xserverrc
-lnk env/xinit.d/xprofile .xprofile
+
+lnk env/tmux/tmux.conf .tmux.conf
+lnk ../../env/i3/config .config/i3/config
 
 popd >/dev/null
