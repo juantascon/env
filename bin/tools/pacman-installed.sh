52646b95803668750256992fb410816b2ad7ff58
juantascon <juantascon@horlux.org>
2018-07-24 12:54:16 -0500

    fixes groups names

--- bin/tools/pacman-installed.sh
+++ bin/tools/pacman-installed.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-groups="base base-devel xorg xorg-drivers plasma-meta gnome xfce4 texlive-most libreoffice"
+groups="base base-devel xorg xorg-drivers plasma gnome xfce4 texlive-most libreoffice"
 
 comm -23 <(pacman -Qetqn|sort) <(pacman -Qgq $groups|sort)
