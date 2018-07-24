8dc9a753bb7eb768b2e9036f1f636b2aa653ad27
juantascon <juantascon@horlux.org>
2018-07-24 12:55:30 -0500

    xrandr.sh asks before setting

--- bin/xrandr.sh
+++ bin/xrandr.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
-available=$(xrandr|awk '/ connected/ {print $1}')
-next=$(xrandr|awk '/ connected \(.*\)/ {print $1}')
+choices=$(xrandr|awk '/ connected/ {print $1}')
+choice=$(echo "${choices}"| rofi -l $(echo "${choices}"|wc -l) -hide-scrollbar -dmenu -p "select entry:")
+
+[ -z "$choice" ] && exit 0
 
 cmd="xrandr "
 
-for m in $available; do
-    if [ "$m" == "$next" ]; then
+for m in $choices; do
+    if [ "$m" == "$choice" ]; then
         mode="--auto"
     else
         mode="--off"
