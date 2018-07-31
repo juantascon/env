3d277005c8808ba38c9e6557575f22c1044506d4
juantascon <juantascon@horlux.org>
2018-07-31 13:50:10 -0500

    better install.sh script

--- install.sh
+++ install.sh
@@ -6,22 +6,24 @@ lnk() {
   
   rm -rf "${dest}"
   mkdir -p $(dirname ${dest})
-  ln -s ${orig} ${dest}
+  ln -s -r ${orig} ${dest}
 }
 
 pushd ~/ >/dev/null
 
 lnk env/bash.d/bashrc .bashrc
 lnk env/bash.d/bashrc .bash_profile
-lnk ../../env/fish.d/config.fish .config/fish/config.fish
-
-lnk env/python/pythonrc .pythonrc
-lnk env/vim .vim
+lnk env/fish.d/config.fish .config/fish/config.fish
 
 lnk env/xinit.d/xinitrc .xinitrc
 lnk env/xinit.d/xserverrc .xserverrc
 
+lnk env/python/pythonrc .pythonrc
+
+lnk env/vim .vim
 lnk env/tmux/tmux.conf .tmux.conf
-lnk ../../env/i3/config .config/i3/config
+lnk env/i3/config .config/i3/config
+lnk env/sxhkd/sxhkdrc .config/sxhkd/sxhkdrc
+lnk env/dunst/dunstrc .config/dunst/dunstrc
 
 popd >/dev/null
