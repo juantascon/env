352faac998279209426e52e8ec59c268684d3b9d
juantascon <juantascon@horlux.org>
2019-06-30 14:35:36 +0200

    uses supervise.sh to supervise desktop apps

---
+++ bin/supervise.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+[ -z "1" ] && echo "usage: $0 <cmd>" && exit 1
+
+while true; do
+    "$@"
+    sleep 1
+done
+
--- bin/term.sh
+++ bin/term.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-while true; do
-    xrdb -merge $HOME/env/urxvt/Xresources
-    SHELL=tmux /usr/bin/urxvt -name urxvt
-done
-
+xrdb -merge $HOME/env/urxvt/Xresources
+SHELL=tmux /usr/bin/urxvt -name urxvt
