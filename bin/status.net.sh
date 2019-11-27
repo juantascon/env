9506691212276df9f45a9213e4bd3fee106db673
juantascon <juantascon@horlux.org>
2019-11-27 12:31:50 +0100

    removes old status scripts

--- bin/status.net.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-icon_on="✓"
-icon_off="✗"
-
-raw_routes=$(ip route list 0/0|grep -o "dev.*"|awk '{print $2}')
-routes=$(echo $raw_routes |tr " " "\n" |sed "s/^en.*/eth/g" |sed "s/^wl.*/wifi/g" |sed "s/^hxvpn.*/hxvpn/g")
-echo "routes: $(echo $routes)"
