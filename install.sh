a4ef6f4a9690f629aece87a90f41faf734d959df
juantascon <juantascon@horlux.org>
2019-11-29 12:33:14 +0100

    installs only shell configs

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ lnk() {
   
   rm -rf "${dest}"
   mkdir -p $(dirname ${dest})
-  ln -s -r ${orig} ${dest}
+  ln -s -r -v ${orig} ${dest}
 }
 
 pushd ~/ >/dev/null
@@ -14,11 +14,7 @@ pushd ~/ >/dev/null
 lnk env/bash.d/bashrc .bashrc
 lnk env/bash.d/bashrc .bash_profile
 lnk env/fish.d/config.fish .config/fish/config.fish
-
-lnk env/xinit.d/xinitrc .xinitrc
-lnk env/xinit.d/xserverrc .xserverrc
-
+lnk env/fish.d/completions .config/fish/completions
 lnk env/vim .vim
-lnk env/tmux/tmux.conf .tmux.conf
 
 popd >/dev/null
