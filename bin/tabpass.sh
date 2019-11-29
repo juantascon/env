f64f78a3485a1c26ce5f59a372dc482afe7ae655
juantascon <juantascon@horlux.org>
2019-11-29 12:32:25 +0100

    improves tabpass using gopass

--- bin/tabpass.sh
+++ bin/tabpass.sh
@@ -1,47 +1,43 @@
 #! /bin/bash
 
 error(){
-    rofi -e "$@"
-    exit 1
+  rofi -e "$@"
+  exit 1
 }
 
 windowname=$(xdotool getactivewindow getwindowname)
 windowname_r=$(echo $windowname| tr " " "\n" | tac | tr "\n" " ")
 browser=$(echo $windowname | awk 'NF{ print $NF }')
 case $browser in
-    # extension: https://addons.mozilla.org/en-US/firefox/addon/hostname-in-title/
-    "Firefox") url=$(echo $windowname_r| awk '{print $4}') ;;
-    # extension: https://chrome.google.com/webstore/detail/url-in-title/ignpacbgnbnkaiooknalneoeladjnfgb
-    "Chromium"|"Vivaldi") url=$(echo $windowname_r| awk '{print $3}') ;;
-    *) error "window not supported: $browser";;
+  # extension: https://addons.mozilla.org/en-US/firefox/addon/hostname-in-title/
+  "Firefox") url=$(echo $windowname_r| awk '{print $4}') ;;
+  # extension: https://chrome.google.com/webstore/detail/url-in-title/ignpacbgnbnkaiooknalneoeladjnfgb
+  "Chromium"|"Vivaldi") url=$(echo $windowname_r| awk '{print $3}') ;;
+  *) error "window not supported: $browser";;
 esac
 
-prefix=${PASSWORD_STORE_DIR-~/.password-store}
-password_list=( "$prefix"/**/*.gpg )
-password_list=( "${password_list[@]#"$prefix"/}" )
-password_list=( "${password_list[@]%.gpg}" )
-
 choices=""
-for password in ${password_list[@]}; do
-    if [[ "$(basename $password)" == ${url}* ]]; then
-        choices="${choices}"$(pass $password|tail -n +3|awk '$1 ~ "^.+:" {sub(":", "", $1); print "'${password}' - "$1}')"\n"
-        choices="${choices}${password} - password\n"
-    fi
-done
+entries=$(gopass ls -f |grep "${url}")
+for entry in ${entries[@]}; do
+  choices="${choices}${entry} - password\n"
+  keys=$(gopass show ${entry}|tail -n +3|awk '{print $1}'|tr -d ":")
 
-[ -z "${choices}" ] && error "match not found: ${url}"
+  for key in $keys; do
+    choices="${choices}${entry} - ${key}\n"
+  done
+done
 choices=$(echo -e "${choices}"|grep -v "^$")
 
+[ -z "${choices}" ] && error "match not found: ${url}"
+
 choice=$(echo "${choices}"| rofi -l $(echo "${choices}"|wc -l) -hide-scrollbar -dmenu -no-custom -p "select entry:")
-entry=${choice% - *}
-field=${choice#* - }
+choice_entry=${choice% - *}
+choice_key=${choice#* - }
 
-if [[ "${field}" == "password" ]]; then
-    result=$(pass ${entry} 2>/dev/null | head -n 1)
-else
-    result=$(pass ${entry} 2>/dev/null | awk '$1 == "'${field}':" {print $2;}')
-fi
+case "${choice_key}" in
+  "password") result=$(gopass show -o ${choice_entry}) ;;
+  "otp") result=$(gopass otp -o ${choice_entry}) ;;
+  *) result=$(gopass show ${choice_entry} ${choice_key}) ;;
+esac
 
-if [ ! -z "${result}" ]; then
-    xdotool - <<<"type --clearmodifiers -- ${result}"
-fi
+xdotool - <<<"type --clearmodifiers -- ${result}"
