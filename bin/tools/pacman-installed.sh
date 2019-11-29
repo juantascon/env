bf6f501047b8b6419bb7d086dca84c7322040ee8
juantascon <juantascon@horlux.org>
2019-11-29 12:36:23 +0100

    removes obsolete group libreoffice

--- bin/tools/pacman-installed.sh
+++ bin/tools/pacman-installed.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-groups="base base-devel xorg xorg-drivers plasma gnome xfce4 texlive-most libreoffice"
+groups="base base-devel xorg xorg-drivers plasma gnome xfce4 texlive-most"
 
 comm -23 <(pacman -Qetqn|sort) <(pacman -Qgq $groups|sort)
