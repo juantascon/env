f27b19eba21123e3a9cbe253b289907b5e8c9bfd
juantascon <juantascon@horlux.org>
2019-12-03 17:03:23 +0100

    different approach to TODO

--- fish.d/x1.juan.fish
+++ fish.d/x1.juan.fish
@@ -1,5 +1,4 @@
-#set -x LD_PRELOAD "/usr/lib/libtrash.so"
 source ~/.asdf/asdf.fish
 shuf -n 1 ~/1000words
-todo.sh
+[ -f ~/TODO ]; and cat ~/TODO
 [ (tty) = "/dev/tty1" ]; and exec xinit # auto xinit on tty1
