e080dccd6faf9cfbab2ff26e29168829f6eb3f6a
juantascon <juantascon@horlux.org>
2020-02-20 12:32:22 +0100

    better fish prompt

--- fish.d/functions/fish_prompt.fish
+++ fish.d/functions/fish_prompt.fish
@@ -1,3 +1,9 @@
+function fish_prompt_echo
+  set_color "$argv[1]"
+  echo -ne "$argv[2]"
+  set_color normal
+end
+
 function fish_prompt
   set -l prompt_status $status
 
@@ -5,12 +11,19 @@ function fish_prompt
 
   set -l prompt_color_cwd cyan
   set -l prompt_color_hostname yellow
-  set -l prompt_color_user cyan
+  set -l prompt_color_user purple
   set -l prompt_color_arrow green
 
   if test $USER = "root"; set prompt_color_user red; end
   if test $hostname != "x1"; set prompt_color_hostname red; end
   if test $prompt_status != 0; set prompt_color_arrow red; end
 
-  printf '%s%s@%s%s %s%s %s➜%s ' (set_color $prompt_color_user) (echo $USER) (set_color $prompt_color_hostname) (echo $hostname) (set_color $prompt_color_cwd) (echo $prompt_cwd) (set_color $prompt_color_arrow) (set_color normal)
+  fish_prompt_echo $prompt_color_user $USER
+  echo -ne "@"
+  fish_prompt_echo $prompt_color_hostname $hostname
+  echo -ne " "
+  fish_prompt_echo $prompt_color_cwd $prompt_cwd
+  echo -ne " "
+  fish_prompt_echo $prompt_color_arrow ➜
+  echo -ne " "
 end
