1e6624e9cfa39c6a12772583768b338020b0c407
juantascon <juantascon@horlux.org>
2020-02-20 11:36:27 +0100

    removes unused tools

--- bin/tools/compress-imgs.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-find -type f -iname "*.jpg"|while read x; do
-  cwebp -q 80 "$x" -o $(dirname $x)/$(basename $x .jpg).webp
-done
--- bin/tools/compress-vids.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-vid="$1"
-ffmpeg -y -i $vid -c:v libx264 -preset veryslow -crf 23 -c:a libmp3lame -q:a 2 ${vid:0:$((${#vid} -4))}.mp4 >> log 2>&1
