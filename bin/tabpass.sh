016746bb51bbe56e862b2cd2012d8456b9bea798
juantascon <juantascon@horlux.org>
2020-02-20 11:31:48 +0100

    minor fixes for tabpass.sh

--- bin/tabpass.sh
+++ bin/tabpass.sh
@@ -19,23 +19,22 @@ esac
 choices=""
 entries=$(gopass ls -f |grep "${url}")
 for entry in ${entries[@]}; do
-  choices="${choices}${entry} - password\n"
   keys=$(gopass show ${entry}|tail -n +3|awk '{print $1}'|tr -d ":")
-
   for key in $keys; do
-    choices="${choices}${entry} - ${key}\n"
+    choices="${choices}${entry}: ${key}\n"
   done
+  choices="${choices}${entry}: pass\n"
 done
 choices=$(echo -e "${choices}"|grep -v "^$")
 
 [ -z "${choices}" ] && error "match not found: ${url}"
 
 choice=$(echo "${choices}"| rofi -l $(echo "${choices}"|wc -l) -hide-scrollbar -dmenu -no-custom -p "select entry:")
-choice_entry=${choice% - *}
-choice_key=${choice#* - }
+choice_entry=${choice%: *}
+choice_key=${choice#*: }
 
 case "${choice_key}" in
-  "password") result=$(gopass show -o ${choice_entry}) ;;
+  "pass") result=$(gopass show -o ${choice_entry}) ;;
   "totp") result=$(gopass otp -o ${choice_entry}) ;;
   *) result=$(gopass show ${choice_entry} ${choice_key}) ;;
 esac
