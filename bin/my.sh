814e75e2671ed9a56d7c066710275008f410b127
juantascon <juantascon@horlux.org>
2020-06-25 08:52:29 +0200

    uses gocryptfs instead of encfs

--- bin/my.sh
+++ bin/my.sh
@@ -1,12 +1,3 @@
-#! /usr/bin/env bash
-
+#!/bin/sh
 # auth optional pam_exec.so expose_authtok /home/juan/env/bin/my.sh
-
-[ $(whoami) != "juan" ] && exit 0
-
-RAW=/home/juan/.my.encfs
-DATA=/home/juan/my
-
-if ! (mount|grep $DATA &>/dev/null); then
-  encfs --stdinpass $RAW $DATA
-fi
+[ $PAM_USER = "juan" ] && gocryptfs -nosyslog /home/juan/.my.gocryptfs /home/juan/my
