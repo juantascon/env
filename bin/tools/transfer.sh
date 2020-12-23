363189f90b76add2729c74245060d50bcf3d2771
juantascon <juantascon@horlux.org>
2020-12-23 04:21:11 +0100

    minor fixes to transfer.sh

--- bin/tools/transfer.sh
+++ bin/tools/transfer.sh
@@ -2,10 +2,11 @@
 
 if [ $# -eq 0 ]; then
     echo -e "usage: $(basename $0) <file> ..."
-    return 1
+    exit 1
 fi
 
 for file in "$@"; do
     basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
     curl --progress-bar --upload-file "$1" "https://transfer.sh/${basefile}"
+    echo
 done
