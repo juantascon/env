259fc63425dd3a6d37f5000417b0dc85c95a36f1
juantascon <juantascon@horlux.org>
2021-04-20 11:54:42 +0200

    i3: adds key binding to move to next/prev workspace

---
+++ xinit.d/services/i3/i3tool.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+cws() {
+  i3-msg -t get_workspaces | jq -r ". [] | select (.focused == true) | .num"
+}
+
+move_to() {
+  [[ "$1" -lt 1 || "$1" -gt 4 ]] && return
+  i3-msg "move container to workspace number $1"
+  i3-msg "workspace $1"
+}
+
+case $1 in
+  "cws") cws ;;
+  "to_prev") move_to $(( $(cws) - 1 )) ;;
+  "to_next") move_to $(( $(cws) + 1 )) ;;
+  *) echo "usage: $0 <cmd>" ;;
+esac
