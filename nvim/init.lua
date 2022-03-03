93803d24b2c1fef94f56a160542ded4fc3c68691
juantascon <juantascon@horlux.org>
2022-03-03 11:19:13 +0100

    adds softtabstop and expandtab

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,6 +26,8 @@ vim.opt.timeoutlen = 250
 vim.opt.undofile = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 1
+vim.opt.expandtab = true
 vim.opt.scrolloff = 10
 vim.opt.list = true
 vim.opt.listchars:append("space:·")
