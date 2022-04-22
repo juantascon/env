49f500b39b62ca291ceeee179d047293d1447dd2
juantascon <juantascon@horlux.org>
2022-04-22 16:13:56 +0200

    my.sh uses another dir

--- bin/my.sh
+++ bin/my.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # auth optional pam_exec.so expose_authtok /juan/env/bin/my.sh
-[ $PAM_USER = "juan" ] && gocryptfs -nosyslog -allow_other ~juan/.my.gocryptfs ~juan/my
+[ $PAM_USER = "juan" ] && gocryptfs -nosyslog -allow_other ~juan/.my.fs ~juan/my
