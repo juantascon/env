083b0dc7150ab20b82d7f301788f461eba84f270
juantascon <juantascon@horlux.org>
2022-08-11 10:38:20 +0200

    adds lsp_lines

--- nvim/init.lua
+++ nvim/init.lua
@@ -269,6 +269,14 @@ require "dep" {
     requires = {"williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim"},
   },
   {
+    "whynothugo/lsp_lines.nvim",
+    function()
+      require("lsp_lines").setup()
+      vim.diagnostic.config({virtual_text = false,})
+    end,
+    url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  },
+  {
     "folke/trouble.nvim",
     function()
       require("trouble").setup {}
