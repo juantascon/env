0ee67c8b1e205aaa73f8259807c69eb312e38332
juantascon <juantascon@horlux.org>
2022-08-11 10:42:42 +0200

    replaces ponymix with pulsemixer

--- bin/noti.sh
+++ bin/noti.sh
@@ -5,8 +5,10 @@ set -e
 case $1 in
   "volume")
     icon="audio-volume-high"
-    ( ponymix is-muted ) && icon="audio-volume-muted"
-    notify-send -i $icon -h int:value:$(ponymix get-volume) $(ponymix defaults --short|head -n 1 |awk '{print $NF}') ;;
+    [[ $( pulsemixer --get-mute ) == "1" ]] && icon="audio-volume-muted"
+    name=$(pulsemixer --list-sinks|grep Default|grep -oP "(?<=Name: ).*(?=, M)")
+    value=$(pulsemixer --get-volume |cut -d" " -f 1)
+    notify-send -i $icon -h int:value:$value "$name" ;;
   "brightness")
     notify-send -i video-display -h int:value:$(brillo -G) $(brillo -L) ;;
   "playerctl")
