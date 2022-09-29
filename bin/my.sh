64011b67d55abb2e43c1fe5fe188b6be742f9c25
juantascon <juantascon@horlux.org>
2022-09-29 09:21:00 +0200

    my.sh rewritten for bash

--- bin/my.sh
+++ bin/my.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 # auth optional pam_exec.so expose_authtok /juan/env/bin/my.sh
-[ $PAM_USER = "juan" ] && gocryptfs -nosyslog -allow_other ~juan/.my.fs ~juan/my
+[ "$PAM_USER" == "juan" ] && gocryptfs -nosyslog -allow_other ~juan/.my.fs ~juan/my
