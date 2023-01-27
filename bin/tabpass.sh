dee116fe4438cc766d51f4403722356f54653fa1
juantascon <juantascon@horlux.org>
2023-01-27 13:01:05 +0100

    reworks pfs list into pfs find

--- bin/tabpass.sh
+++ bin/tabpass.sh
@@ -4,17 +4,8 @@ get_browser_url() {
   xdotool getactivewindow getwindowname| sd "qutebrowser: " ""
 }
 
-pfs_find() {
-  url=$1
-  for key in $(pfs list); do
-    if [[ ${url} == *${key} ]]; then
-      echo ${key}
-    fi
-  done
-}
-
 url=$(get_browser_url)
-ids=$(pfs_find $url)
+ids=$(pfs find --mode prefix $url)
 options=$(pfs select $ids)
 index=$(echo "$options" | cut -f 1,2 -d " " | rofi -dmenu -matching fuzzy -format d -no-custom -p "select:")
 [ -z "$index" ] && exit 1
