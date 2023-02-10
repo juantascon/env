4e3872cb62cb0cde3a25f9b1b6d529ec28324599
juantascon <juantascon@horlux.org>
2023-02-10 15:45:21 +0100

    updates tabpass to use the new pfs

--- bin/tabpass.sh
+++ bin/tabpass.sh
@@ -1,16 +1,27 @@
 #! /bin/bash
 
-get_browser_url() {
-  xdotool getactivewindow getwindowname| sd "qutebrowser: " ""
+get_url_qutebrowser() {
+  xdotool getactivewindow getwindowname | sd "qutebrowser: " ""
 }
 
-url=$(get_browser_url)
-ids=$(pfs find --mode prefix $url)
-options=$(pfs select $ids)
-index=$(echo "$options" | cut -f 1,2 -d " " | rofi -dmenu -matching fuzzy -format d -no-custom -p "select:")
-[ -z "$index" ] && exit 1
-row=$(echo "$options" | head -n $index | tail -n 1)
-field=$(echo "$row" | cut -f 2 -d " ")
-value=$(echo "$row" | cut -f 3 -d " ")
-[[ $field == *totp ]] && value=$(oathtool --totp --base32 $value)
+get_url_edge() {
+  xdotool getactivewindow getwindowname | cut -d"-" -f 1
+}
+
+clean_url() {
+  url="$1"
+  url=${url/#http://}
+  url=${url/#https://}
+  url=${url/#www.}
+  url=${url/#accounts.}
+  echo $url
+}
+
+url=$(get_url_edge)
+url=$(clean_url $url)
+choice=$(pfs --multi $url | rofi -dmenu -matching fuzzy -format d -no-custom -p "select:")
+read -r id field <<< $(echo $choices)
+value=$(pfs $id $field)
+[ $? -ne 0 ] && exit 1
+[[ $field == totp ]] && value=$(oathtool --totp --base32 $value)
 xdotool type --clearmodifiers -- $value
