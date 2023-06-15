2b7c3c973d5a64b83fc3bb149280fec237e4bb6e
juantascon <juantascon@horlux.org>
2023-06-15 12:59:07 +0200

    updates config to use btp.sh

--- bin/noti.sh
+++ bin/noti.sh
@@ -5,15 +5,15 @@ set -e
 case $1 in
   "volume")
     icon="audio-volume-high"
-    [[ $( pulsemixer --get-mute ) == "1" ]] && icon="audio-volume-muted"
+    [[ $(pulsemixer --get-mute) == "1" ]] && icon="audio-volume-muted"
     name=$(pulsemixer --list-sinks|grep Default|grep -oP "(?<=Name: ).*(?=, M)")
     value=$(pulsemixer --get-volume |cut -d" " -f 1)
     notify-send -i $icon -h int:value:$value "$name" ;;
   "profile")
-    notify-send -i microphone-sensitivity-medium-symbolic $(bluetooth-profile.sh get) ;;
+    notify-send -i microphone-sensitivity-medium-symbolic $(btp.sh get) ;;
   "brightness")
     notify-send -i video-display -h int:value:$(brillo -G) $(brillo -L) ;;
   "playerctl")
-    notify-send -i multimedia-player $(playerctl -l) $(sleep 0.2 && playerctl status) ;;
+    notify-send -i multimedia-player $(playerctl -l) $(playerctl status) ;;
   *) echo "usage: $0 <type>"; exit 1;;
 esac
