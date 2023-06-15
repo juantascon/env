bdd913ccbdb4cb3be52815e273c90adf7024a787
juantascon <juantascon@horlux.org>
2023-06-15 12:59:37 +0200

    updates pfs completion

--- fish.d/completions/pfs.fish
+++ fish.d/completions/pfs.fish
@@ -1,7 +1,2 @@
-set -l cmds get find select
-
-complete -f -c pfs -n "not __fish_seen_subcommand_from $cmds" -a get
-complete -f -c pfs -n "not __fish_seen_subcommand_from $cmds" -a find
-complete -f -c pfs -n "not __fish_seen_subcommand_from $cmds" -a select
-
-complete -f -c pfs -n "__fish_seen_subcommand_from get; and test (__fish_number_of_cmd_args_wo_opts) = 2" -a "(pfs find)"
+complete -c pfs -n "not __fish_seen_subcommand_from (pfs -m -i)" -a "-h --help -m --multi -i --ids-only"
+complete -c pfs -n "not __fish_seen_subcommand_from (pfs -m -i)" -a "(pfs -m -i)"
