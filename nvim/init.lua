8a9bb13d0f1e4d4fbf665f639d8d22b40d8bcccd
juantascon <juantascon@horlux.org>
2023-09-06 12:13:49 +0200

    removes noice.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -154,24 +154,6 @@ require("lazy").setup ({
     end
   },
   {
-    "folke/noice.nvim",
-    dependencies = {
-      {"MunifTanjim/nui.nvim"},
-      -- {"rcarriga/nvim-notify"},
-    },
-    config = function()
-      require("noice").setup({
-        lsp = {
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
-          },
-        },
-      })
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
     priority = 1000,
     lazy = false,
