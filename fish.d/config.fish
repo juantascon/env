d7e6e3b69148a1fc4c73152a448d7a6277399801
juantascon <juantascon@horlux.org>
2023-09-25 13:23:00 +0200

    replaces direnv and asdf with rtx

--- fish.d/config.fish
+++ fish.d/config.fish
@@ -1,4 +1,4 @@
-set -x PATH ~/env/bin /usr/bin ~/.asdf/bin ~/.pipx/bin
+set -x PATH ~/env/bin ~/.pipx/bin /usr/bin
 set -x LANG en_US.UTF-8
 set -x TERM xterm-256color
 
@@ -11,7 +11,7 @@ set -u fish_greeting #makes fish silent
 bind \cH backward-kill-word
 
 xdg-dirs | source
-direnv hook fish | source
+rtx activate fish | source
 keychain --quiet --eval --agents ssh id_rsa | source
 
 [ "$USER" = "juan" ] && [ (tty) = "/dev/tty1" ] && [ -z "$DISPLAY" ] && exec dbus-run-session xinit
